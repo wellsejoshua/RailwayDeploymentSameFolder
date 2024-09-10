5bbe47f669f32a38aa9c986eaa3c3b740d452704
coded <codedbywells@gmail.com>
2024-09-09 23:35:40 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RailwayDeploymentSameFolder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
